44ce155768090ef46901ab087f421b3f6b716bd2
NekinKirezaya <nekinkirezaya@gmail.com>
2022-10-11 14:15:58 +0200

    Project 2nd day

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -8,7 +8,7 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 function Table() {
-    
+
     const [name, setName] = useState({
         name: 'name',
         surname: 'surname',
