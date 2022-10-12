e596d6a2df42fe3b7ee0c6d8527779e1f4619034
NekinKirezaya <nekinkirezaya@gmail.com>
2022-10-12 22:52:32 +0200

    project
    update css

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,50 +1,53 @@
 import React, { useState, useEffect } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 
 function TableProject() {
 
     const [item, setItem] = useState([])
 
     const callApi = () => {
-    fetch('https://api.randomuser.me/')
-        .then((response) => console.log(response.json()))
+        fetch('https://api.randomuser.me/')
+            .then((response) => console.log(response.json()))
             // .then((item) => setItem(item.all))
-        .catch((error) => console.log(error));
+            .catch((error) => console.log(error));
     }
-        
 
+    const user = [{   
+        results: []
+
+    }]
+    
 
     useEffect(() => {
         callApi()
     }, [])
 
     return (
-        <>
-            <TableContainer component={Paper}>
-                <Table>
-                    <TableHead>
-                        <TableRow className={callApi}>
-                            <TableCell>id</TableCell>
-                            <TableCell>name</TableCell>
-                            <TableCell>Age</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        <TableRow>
-                            <TableCell >{item}</TableCell>
-                        </TableRow>
-                    </TableBody>
-                </Table>
-            </TableContainer>
-             
-        </>
-    )
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>
+                {user.map((item)=>
+                    <tr>
+                        <td>{item.results}</td>
+                        <td></td>
+                    </tr>
+                    )
+                }
+              </td>
+
+            </tr>
+          </tbody>
+        </table>
+      );
 
 }
 
